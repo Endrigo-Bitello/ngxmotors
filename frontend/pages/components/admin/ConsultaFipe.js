b51e6f1b37c2ca7ba0005241867ab0b838124de0
Endrigo Bitello <endrigoluizbitello@gmail.com>
2024-10-24 19:21:14 -0300

    Correção na data da consulta fipe

--- frontend/pages/components/admin/ConsultaFipe.js
+++ frontend/pages/components/admin/ConsultaFipe.js
@@ -196,7 +196,7 @@ const ConsultaFipe = () => {
         </h4>
         <p className="text-2xl font-bold text-gray-900 mt-2">{data.Valor}</p>
         <p className="text-sm text-gray-500 mt-1">
-          Data da consulta: {data.DataConsulta}
+          Data da consulta: {new Date().toLocaleString()}
         </p>
       </div>
     </div>
